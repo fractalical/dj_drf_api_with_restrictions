cbbc1d41252967dd1703c351f164f7043000092f
Artur <ArthurZ43@yandex.ru>
2023-03-08 16:57:29 +0300

    added status filter

--- advertisements/filters.py
+++ advertisements/filters.py
@@ -13,4 +13,4 @@ class AdvertisementFilter(filters.FilterSet):
 
     class Meta:
         model = Advertisement
-        fields = ['creator', 'created_at']
+        fields = ['creator', 'created_at', 'status']
